91f38956ccd32bdaa51a10412629a7e0fceb4892
dianemarie-public <69361953+dianemarie-public@users.noreply.github.com>
2020-11-16 18:26:51 -0600

    Create employees.sql

---
+++ EmployeeSQL/employees.sql
@@ -0,0 +1,83 @@
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/kb9hyh
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- Modify this code to update the DB schema diagram.
+-- To reset the sample schema, replace everything with
+-- two dots ('..' - without quotes).
+
+CREATE TABLE "employees" (
+    "emp_no" integer NOT NULL,
+    "emp_title" VARCHAR(30) NOT NULL,
+    "birth_date" VARCHAR(30) NOT NULL,
+    "first_name" VARCHAR(30) NOT NULL,
+    "last_name" VARCHAR(30) NOT NULL,
+    "sex" VARCHAR(30) NOT NULL,
+    "hire_date" VARCHAR(30) NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no", "emp_title" 
+     )
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" integer   NOT NULL,
+    "salary" money   NOT NULL,
+    CONSTRAINT "pk_salaries" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "departments" (
+    "dept_no" VARCHAR(30) NOT NULL,
+    "dept_name" VARCHAR(30) NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "dept_manager" (
+    "dept_no" VARCHAR(30) NOT NULL,
+    "emp_no" integer   NOT NULL,
+    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
+        "dept_no","emp_no"
+     )
+);
+
+CREATE TABLE "dept_emp" (
+    "emp_no" integer   NOT NULL,
+    "dept_no" VARCHAR(30) NOT NULL,
+    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
+        "emp_no","dept_no"
+     )
+);
+
+CREATE TABLE "titles" (
+    "title_id" VARCHAR(30) NOT NULL,
+    "title" VARCHAR(30) NOT NULL,
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+SELECT * FROM employees;
+
+SELECT employees.emp_no, salaries.emp_no
+FROM salaries
+INNER JOIN employees ON
+employees.emp_no=salaries.emp_no;
+
+SELECT * FROM salaries;
+
+SELECT employees.emp_title, titles.title_id
+FROM titles
+INNER JOIN employees ON
+employees.emp_title=titles.title_id;
+
+SELECT * FROM titles;
+
+SELECT departments.dept_no, dept_manager.dept_no
+FROM dept_manager
+INNER JOIN departments ON
+departments.dept_no=dept_manager.dept_no
+
+SELECT * FROM departments;
