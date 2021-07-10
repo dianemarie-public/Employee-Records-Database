86c60310e175026a196bee3f43d618d4fe476cd9
dianewitt <dianemarieak@gmail.com>
2021-07-10 12:38:16 -0500

    Moves files to folder

--- DatabaseDesign/QuickDBD-Free Diagram.sql
+++ DatabaseDesign/QuickDBD-Free Diagram.sql
@@ -1,10 +1,5 @@
 ﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- Link to schema: https://app.quickdatabasediagrams.com/#/d/kb9hyh
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
-
--- Modify this code to update the DB schema diagram.
--- To reset the sample schema, replace everything with
--- two dots ('..' - without quotes).
 
 CREATE TABLE "employees" (
     "emp_no" integer   NOT NULL,
